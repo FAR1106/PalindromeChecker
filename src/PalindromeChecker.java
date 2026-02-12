7c59a0d929d0739fdd1be0db99a725f19796d91b
FAR1106 <ma5481@srmist.edu.in>
2026-02-12 10:25:42 +0530

    UC1: Application Entry & Welcome Message

---
+++ src/PalindromeChecker.java
@@ -0,0 +1,10 @@
+public class PalindromeChecker {
+    /*
+        UC1
+    */
+    public static void main(String[] args){
+        System.out.println("Welcome to the Palindrome Checker Management System\n");
+        System.out.println("Version : 1.0");
+        System.out.println("System initialized successfully");
+    }
+}
